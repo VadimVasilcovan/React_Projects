86a0c3915c3e606f154ec911610a02b2736cb3fe
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2024-12-14 23:13:23 +0200

    Color Generator Project.

--- React_Projects/src/App.jsx
+++ React_Projects/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
+import RandomColorGenerator from './Random_Color_Generator/Color_Generator,'
 
 function App() {
  
 
   return (
     <>
-    
-      
+    <RandomColorGenerator/>
     </>
   )
 }
