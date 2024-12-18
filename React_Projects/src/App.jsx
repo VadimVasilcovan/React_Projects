9c33acdb8e45e239e73acc61b029c70fd80bc19a
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2024-12-18 23:35:26 +0200

    Load_More_Data

--- React_Projects/src/App.jsx
+++ React_Projects/src/App.jsx
@@ -5,6 +5,7 @@ import RatingStarex03 from "./Star_Rating/Star-Ratings.0.3";
 import StarRating from "./Star_Rating/Star_Rating";
 import StarRatingEx from "./Star_Rating/Star_Rating.0.2";
 import RatingStarEx04 from "./Star_Rating/Star_Rating.0.4";
+import ImportMoreItems from "./load_more_items/load_more_items";
 
 function App() {
   return (
@@ -16,7 +17,10 @@ function App() {
         page={"10"}
       />*/} 
 
-      <RatingStarEx04 numberOfStars={8}/>
+      {/*<RatingStarEx04 numberOfStars={8}/>*/}
+      
+      {/*Load more products components*/}
+      <ImportMoreItems/>
     </>
   );
 }
---
+++ React_Projects/src/load_more_items/load_more_items.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const ImportMoreItems = () => {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+
+  async function fetchProducts() {
+    try {
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+
+      const result = await response.json();
+
+      console.log(result);
+    } catch (e) {}
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return <div className="continer"></div>;
+};
+
+export default ImportMoreItems;
