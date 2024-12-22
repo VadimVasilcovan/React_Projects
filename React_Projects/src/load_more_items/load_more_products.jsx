43e1d30641ea52fc2f125f229338a7b7ecb6c2d4
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2024-12-23 01:50:37 +0200

    ex.23.12.2024

--- React_Projects/src/load_more_items/load_more_products.jsx
+++ React_Projects/src/load_more_items/load_more_products.jsx
@@ -2,65 +2,66 @@ import { useState, useEfect } from "react";
 import "../load_more_items/load_more_items.css";
 
 const ImportMoreProducts = () => {
-    const [loading, setLoading]= useState(false);
-    const [products, setProducts]= useState([]);
-    const [count, setCount]=useState(0)
-    const[disableButton, setDisableButton] =useState(false);
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
 
-    async function fetchProducts (){
-        try{
-            setLoading(true);
-            const response = await fetch(
-                `https://dummyjson.com/products?limit=20&skip=${
-                    count === 0 ? 0 : count *20
-                }`
-            );
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
 
-            const result = await response.json();
-            if (result && result.products && result.products.length){
-                setProducts((prevData) => [...prevData , ...result.products]);
-                setLoading(false);
-            } 
-            console.log(result);
-        }catch(e){
-            console.log(e);
-            setLoading(false);
-        }
+      const result = await response.json();
+      if (result && result.products && result.products.length) {
+        setProducts((prevData) => [...prevData, ...result.products]);
+        setLoading(false);
+      }
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
     }
+  }
 
-    useEfect(() => {
-        fetchProducts();
-    },[count]);
-    
-    useEfect(() => {
-        setDisableButton(() => {
-            if(products && products.length === 100) setDisableButton(true);
-        
-        })
-    },[products]);
+  useEfect(() => {
+    fetchProducts();
+  }, [count]);
 
-    if (loading){
-        return <div>Loading data ! please wait.</div>;
-    }
+  useEfect(() => {
+    setDisableButton(() => {
+      if (products && products.length === 100) setDisableButton(true);
+    });
+  }, [products]);
 
-    return(<div className="load-more-container">
-        <div className="product-container">
-            {products && productsLength === 100 ? products.map((item) => (
-                <div className="product-container" key={item.id}>
-                    <img src={item.thumbnail} alt={item.title}/>
-                    <p>{item.title}</p>
-                </div>
-            )):  null}
-        </div>
-        <div className="button-container">
-            <button disabled={disableButton} onClick={() => setCount(count + 1)}>
-                Load More Products
-            </button>
-            {
-                disableButton ? <p>You have reached to 100 product</p> : null
-            }
-        </div>
-    </div>)
-}
+  if (loading) {
+    return <div>Loading data ! please wait.</div>;
+  }
 
-export default ImportMoreProducts
+  return (
+    <div className="load-more-container">
+      <div className="product-container">
+        {products && productsLength === 100
+          ? products.map((item) => (
+              <div className="product-container" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="button-container">
+        <button disabled={disableButton} onClick={() => setCount(count + 1)}>
+          Load More Products
+        </button>
+        {disableButton ? <p>You  reached to 100 product</p> : null}
+      </div>
+    </div>
+  );
+};
+
+export default ImportMoreProducts;
