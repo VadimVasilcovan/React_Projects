9ea15174bbef44ec9225371eaa164df7a2a35948
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-01-11 12:14:42 +0200

    11.01.2025

--- React_Projects/src/load_more_items/load-mor-data.jsx
+++ React_Projects/src/load_more_items/load-mor-data.jsx
@@ -1,32 +1,36 @@
-import { useEffect, useState } from "react"
-
-
+import { useEffect, useState } from "react";
 
 const GetData = () => {
-    const [data, setdata]=useState([])
+  const [data, setdata] = useState([]);
 
-
-    async function fetchData () {
-
-        try{
-            const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-            const result = await response.json();
-            setdata(result);
-        }catch(e){
-            console.log(e)
-        }
+  async function fetchData() {
+    try {
+      const response = await fetch(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      );
+      const result = await response.json();
+      if (result && result.length) {
+        setdata((d) => [...d, ...result]);
+      }
+      {
+        /*setdata(result);*/
+      }
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    useEffect(() => {
-        fetchData();
-    }, [])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  return (
+    <>
+      {data.map((product) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
+    </>
+  );
+};
 
-    return(<>
-    {data.map((product) => (<div key={product.id}>{product.name}</div>))}
-
-
-    </>)
-}
-
-export default GetData
+export default GetData;
