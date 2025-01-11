9815e9d3fe553eb2f3d6fb8cc9b0d78d51ee3282
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-01-11 14:19:12 +0200

    11.01.2025

--- React_Projects/src/load_more_items/load-mor-data.jsx
+++ React_Projects/src/load_more_items/load-mor-data.jsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 
 const GetData = () => {
   const [data, setdata] = useState([]);
+  const [count, setCount] = useState(0)
 
   async function fetchData() {
     try {
       const response = await fetch(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=${count === 0 ? 0 : count * 20}&sparkline=false`
       );
       const result = await response.json();
       if (result && result.length) {
@@ -22,14 +23,17 @@ const GetData = () => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [count]);
 
   return (
-    <>
+    <div>
       {data.map((product) => (
-        <div key={product.id}>{product.name}</div>
+        <div key={product.id}>{product.name}
+        </div>
       ))}
-    </>
+      <button  onClick={() => setCount(count + 1)}>Load more</button>
+      
+    </div>
   );
 };
 
