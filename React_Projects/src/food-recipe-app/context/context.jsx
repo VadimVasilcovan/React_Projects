4d25c9a15f825d8508303f323887e76400a1da1b
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-01-23 23:57:56 +0200

    23.01.2025

--- React_Projects/src/food-recipe-app/context/context.jsx
+++ React_Projects/src/food-recipe-app/context/context.jsx
@@ -4,31 +4,33 @@ export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
   const [searchParam, setSearchParam] = useState("");
-  const [loading, setLoadiing] = useState(false);
-  const [recipeList, setRecipeList] = useState;
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
 
   async function handleSubmit(event) {
     event.preventDefault();
+    setLoading(true);
     try {
       const res = await fetch(
         `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
       );
       const data = await res.json();
       if (data?.data?.recipes) {
-        setRecipeList(data?.data?.recipes);
-        setLoadiing(false);
-        setSearchParam("");
+        setRecipeList(data.data.recipes);
       }
-      console.log(data);
+      setSearchParam("");
     } catch (e) {
       console.log(e);
-      setLoadiing(false);
-      setSearchParam("");
+    } finally {
+      setLoading(false);
     }
   }
+
+  console.log(loading, recipeList);
+
   return (
     <GlobalContext.Provider
-      value={{ searchParam, setSearchParam, handleSubmit }}
+      value={{ searchParam, setSearchParam, handleSubmit, recipeList, loading }}
     >
       {children}
     </GlobalContext.Provider>
--- React_Projects/src/food-recipe-app/home/recipe-app-home.jsx
+++ React_Projects/src/food-recipe-app/home/recipe-app-home.jsx
@@ -1,3 +1,19 @@
+import RecipeItem from "../components/recipe-item-folder/recipep-list";
+import { GlobalContext } from "../context/context";
+import { useContext } from "react";
+
 export default function HomePage() {
-  return <div>Home</div>;
+  const { recipeList, loading } = useContext(GlobalContext);
+
+  if(loading) return<div>Loading...Please wait!</div>
+  return (
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      Home
+      {recipeList && recipeList.length > 0
+        ? recipeList.map((item) => <RecipeItem item={item} />)
+        : <div>
+          <p className="='lg:text-4xl text-xl text-center text-black font-extrabold">Nothing to show. Please search something</p>
+          </div>}
+    </div>
+  );
 }
